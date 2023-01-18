63acc98f12cd868ca860beb4743d76ea6644f049
Andrew <aforest41@yahoo.com>
2023-01-18 09:19:36 -0800

    commit tree 37af5e810ac4c6efe65ce5ba74b8850c7d059da8

--- app/models/dev.rb
+++ app/models/dev.rb
@@ -1,3 +1,10 @@
 class Dev < ActiveRecord::Base
+    has_many :freebies
+    has_many :companies, through: :freebies
+
+    def received_one(item_name)
+        dev = Freebie.find_by(item_name: item_name).dev
+        dev == self 
+    end
 
 end
---
+++ db/migrate/20230117181644_add_column_to_freebie.rb
@@ -0,0 +1,5 @@
+class AddColumnToFreebie < ActiveRecord::Migration[6.1]
+  def change
+    add_column :freebies, :value, :integer
+  end
+end
